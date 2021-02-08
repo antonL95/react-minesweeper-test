b369326b03fee0d3afb586d65132c353a1457cb0
Anton Loginov <loginovanton95@gmail.com>
2021-02-08 16:59:49 +0100

    Init commit.

--- README.md
+++ README.md
@@ -1,70 +1,10 @@
-# Getting Started with Create React App
+First of all I need to create matrix or empty board and the matrix has certain props like mine or if its empty or how
+many mines are around using ```emptyBoard()```.
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+When I have the matrix I will plant mines on random places in the matrix using ```setMinesOnBoard()```.
 
-## Available Scripts
+When I set up all the mines I then need to find out where on the board is cell located for that I am
+using ```examineEachCell()``` to create area around the cell and in that area I am looking for the mines and I add mine
+counter for each mine in the area of the cell however if there is no mine in the area I can tag the cell empty.
 
-In the project directory, you can run:
-
-### `npm start`
-
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
-
-The page will reload if you make edits.\
-You will also see any lint errors in the console.
-
-### `npm test`
-
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
-
-### `npm run build`
-
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
-
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
-
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
-
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
-
-### Code Splitting
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/code-splitting](https://facebook.github.io/create-react-app/docs/code-splitting)
-
-### Analyzing the Bundle Size
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size](https://facebook.github.io/create-react-app/docs/analyzing-the-bundle-size)
-
-### Making a Progressive Web App
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app](https://facebook.github.io/create-react-app/docs/making-a-progressive-web-app)
-
-### Advanced Configuration
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/advanced-configuration](https://facebook.github.io/create-react-app/docs/advanced-configuration)
-
-### Deployment
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/deployment](https://facebook.github.io/create-react-app/docs/deployment)
-
-### `npm run build` fails to minify
-
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+Finally I just render the matrix.
--- src/App.js
+++ src/App.js
@@ -1,25 +1,153 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    let [x, setX] = useState(12);
+    let [y, setY] = useState(x);
+    let [mines, setMines] = useState(20);
+
+    const handelSubmit = (e) => {
+        setX(e.target.elements['x'].value);
+        setY(e.target.elements['x'].value);
+        setMines(e.target.elements['mines'].value);
+        e.preventDefault()
+    }
+
+
+    const emptyBoard = (x = 14, y = 14) => {
+        let board = [];
+        for (let i = 0; i < x; i++) {
+            board.push([]);
+            for (let j = 0; j < y; j++) {
+                board[i][j] = {
+                    mine: false,
+                    x: i,
+                    y: j,
+                    isEmpty: false,
+                    minesNearBy: 0
+                };
+            }
+        }
+        return board;
+    };
+
+    const setMinesOnBoard = (board, numberOfX, numberOfY, mines) => {
+        let c = 0;
+        let randX = 0;
+        let randY = 0;
+        while (c < mines) {
+            randX = Math.floor(Math.random() * numberOfX);
+            randY = Math.floor(Math.random() * numberOfY);
+            if (!board[randX][randY].mine) {
+                board[randX][randY].mine = true
+                c++;
+            }
+        }
+        return board;
+    }
+
+    const examineEachCell = (currentX, currentY, data, x, y) => {
+        const el = [];
+
+        //up
+        if (currentX > 0) {
+            el.push(data[currentX - 1][currentY]);
+        }
+
+        //down
+        if (currentX < y - 1) {
+            el.push(data[currentX + 1][currentY]);
+        }
+
+        //left
+        if (currentY > 0) {
+            el.push(data[currentX][currentY - 1]);
+        }
+
+        //right
+        if (currentY < x - 1) {
+            el.push(data[currentX][currentY + 1]);
+        }
+
+        // top left
+        if (currentX > 0 && currentY > 0) {
+            el.push(data[currentX - 1][currentY - 1]);
+        }
+
+        // top right
+        if (currentX > 0 && currentY < x - 1) {
+            el.push(data[currentX - 1][currentY + 1]);
+        }
+
+        // bottom right
+        if (currentX < y - 1 && currentY < x - 1) {
+            el.push(data[currentX + 1][currentY + 1]);
+        }
+
+        // bottom left
+        if (currentX < y - 1 && currentY > 0) {
+            el.push(data[currentX + 1][currentY - 1]);
+        }
+
+        return el;
+    }
+
+
+    const areaAroundMine = (boardWithMines, x, y) => {
+        let updatedData = boardWithMines;
+
+        for (let i = 0; i < x; i++) {
+            for (let j = 0; j < y; j++) {
+                if (boardWithMines[i][j].mine !== true) {
+                    let mine = 0;
+                    const area = examineEachCell(boardWithMines[i][j].x, boardWithMines[i][j].y, boardWithMines, x, y);
+                    area.map((value) => {
+                        if (value.mine) {
+                            mine++;
+                        }
+                    });
+                    if (mine === 0) {
+                        updatedData[i][j].isEmpty = true;
+                    }
+                    updatedData[i][j].minesNearBy = mine;
+                }
+            }
+        }
+
+        return (updatedData);
+    }
+
+    const renderBoard = (board) => {
+        return board.map(boardRow => {
+            return boardRow.map(cell => {
+                return (
+                    <div>
+                        <div className="cell">
+                            {cell.mine && (<span>x</span>)}
+                            {cell.minesNearBy > 0 && (<span>{cell.minesNearBy}</span>)}
+                        </div>
+                        {(boardRow[boardRow.length - 1] === cell) ? <div className="clear" /> : ""}
+                    </div>
+                )
+            })
+        })
+    }
+
+    let board = emptyBoard(x, y);
+    let boardWithMines = setMinesOnBoard(board, x, y, mines)
+    let finalBoard = areaAroundMine(boardWithMines, x, y)
+
+    return (
+        <div>
+            <form onSubmit={handelSubmit}>
+                <input type="number" name="x" placeholder="board size"/>
+                <input type="number" name="mines"  placeholder="mines" />
+                <input type="submit"/>
+            </form>
+            {renderBoard(finalBoard)}
+        </div>
+    );
 }
 
 export default App;
